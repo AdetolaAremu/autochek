8160a04907a6625a8b142e10c57f0ebfa502a73e
Adetola <aremutola@gmail.com>
2023-10-23 18:16:06 +0100

    to fixed fix on live

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -13,12 +13,12 @@ interface CarListingProps {
 const CarCard: NextPage<CarListingProps> = ({ carList }) => {
   return (
     <div
-      key={carList.id}
+      key={carList?.id}
       className="w-80 md:w-[17.3rem] lg:w-80 xl:w-[18rem] 2xl:w-80 p-1 mt-3 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
     >
       <div className="relative">
         <Image
-          src={carList.imageUrl}
+          src={carList?.imageUrl}
           className={`w-full h-56 bg-cover object-fill ${
             !carList.imageUrl && "blur-sm"
           }`}
@@ -33,7 +33,7 @@ const CarCard: NextPage<CarListingProps> = ({ carList }) => {
             <FontAwesomeIcon icon={faStar} className="text-yellow-500" />
           </div>
           <div className="text-xs text-white pt-1">
-            {carList.gradeScore.toFixed(1)}
+            {carList?.gradeScore?.toFixed(1)}
           </div>
         </div>
       </div>
@@ -42,10 +42,10 @@ const CarCard: NextPage<CarListingProps> = ({ carList }) => {
         <div className="flex justify-between">
           <div>
             <button className="text-xs capitalize bg-[#0579C9] text-white rounded-md px-2">
-              {carList.sellingCondition}
+              {carList?.sellingCondition}
             </button>
             <button className="text-xs ml-2 capitalize bg-[#0579C9] text-white rounded-md px-2">
-              {carList.mileage} {carList.mileageUnit}
+              {carList?.mileage} {carList?.mileageUnit}
             </button>
           </div>
 
@@ -57,19 +57,19 @@ const CarCard: NextPage<CarListingProps> = ({ carList }) => {
         <div className="flex justify-between text-sm mt-1">
           <div className="font-bold">
             {/* {carList.title} */}
-            {carList.title.length > 13
+            {carList?.title.length > 13
               ? `${carList?.title?.slice(0, 13)}...`
               : carList?.title}
             ({carList.year})
           </div>
           <div className="text-gray-600">
-            ₦ {carList.marketplacePrice.toLocaleString()}
+            ₦ {carList?.marketplacePrice?.toLocaleString()}
           </div>
         </div>
         <div className="h-1 bg-[#0579C9] my-1"></div>
         <div className="flex justify-between text-sm mt-1">
-          <div className="font-bold capitalize">{carList.transmission}</div>
-          <div className="text-gray-600 capitalize">{carList.fuelType}</div>
+          <div className="font-bold capitalize">{carList?.transmission}</div>
+          <div className="text-gray-600 capitalize">{carList?.fuelType}</div>
         </div>
         <div className="mt-1">
           <div className="flex justify-between text-sm">
